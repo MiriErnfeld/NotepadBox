9c304d999c66c132397747cbe044766a1d09e6ec
MiriErnfeld <miri@leader.codes>
2021-04-28 12:01:50 +0300

    add component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import './components/styles.css'
 import NotePad from './components/NotePad'
 import ReactStickyNotes from '@react-latest-ui/react-sticky-notes';
+import MyFirstStickyNotes from './components/StickyNote'
 
 function App() {
   return (
     <div className="App">
-      
+      <MyFirstStickyNotes></MyFirstStickyNotes>
       {/* <ReactStickyNotes
         noteWidth={"140px"}
         noteHeight={"140px"}
--- src/components/StickyNote.js
+++ src/components/StickyNote.js
@@ -1,47 +1,25 @@
 import React, { useState } from 'react';
 import ReactStickies from 'react-stickies'; //ES6
 
-export default function MyFirstStickyNotes(){
-const [note, setNote] = useState([])
+export default function MyFirstStickyNotes() {
+    const [note, setNote] = useState([])
 
-function onSave () {
-    // Make sure to delete the editorState before saving to backend
-    const notes = this.state.notes;
-    notes.map(note => {
-      delete note.editorState;
-    })
-    // Make service call to save notes
-    // Code goes here...
-  }
-  onChange (notes) {
-    this.setState({ // Update the notes state
-      notes
-    })
-  }
-}
-
-
-class MyFirstStickyNotes extends Component {
- 
-    onSave () {
-      // Make sure to delete the editorState before saving to backend
-      const notes = this.state.notes;
-      notes.map(note => {
-        delete note.editorState;
-      })
-      // Make service call to save notes
-      // Code goes here...
+    function onSave() {
+        // Make sure to delete the editorState before saving to backend
+        const notes = note;
+        notes.map(note => {
+            delete note.editorState;
+        })
+        // Make service call to save notes
+        // Code goes here...
     }
-    onChange (notes) {
-      this.setState({ // Update the notes state
-        notes
-      })
+    function onChange(notes) {
+        setNote(notes)
     }
     return (
-      <ReactStickies
-        notes={this.state.notes}
-        onChange={this.onChange}
-      />
+        <ReactStickies
+            notes={note}
+            onChange={onChange}
+        />
     )
-  }
-};
+}
