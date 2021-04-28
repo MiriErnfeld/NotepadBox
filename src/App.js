1713581dec3047086f1779896f3e906af17a1c44
MiriErnfeld <miri@leader.codes>
2021-04-28 11:23:54 +0300

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import NotePad from './components/NotePad'
+import ReactStickyNotes from '@react-latest-ui/react-sticky-notes';
 
 function App() {
   return (
     <div className="App">
-      <NotePad></NotePad>
+         <ReactStickyNotes />
+      {/* <NotePad></NotePad> */}
     </div>
   );
 }
