33d4a92ecf4cecffe1ab2fddea7044337a73820c
MiriErnfeld <miri@leader.codes>
2021-05-06 10:22:32 +0300

    --- Auto-Git Commit ---

--- src/components/myNote.js
+++ src/components/myNote.js
@@ -1,29 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './myNote.css'
 import Color from './colorPallete'
+import './colorPallete.css'
 
 export default function Notes() {
-
-    function changeCOlor() {
-
-        
-        <div className="curr" >
-        {mycolors.map((c, i) => {
-            return <div className="divColors " className="colorDiv handPointer"
-                // style={{ backgroundColor: c }}
-                style={{ backgroundColor: c }}
-            >
-            </div>
-        })}
-     
+    const [isClicked, setisClicked] = useState(false)
+    const mycolors = [
+        '#44D7B6', '#40D9ED', '#3598F4', '#8580FD', '#6236FC', '#B620E0', '#FD80E5', '#6DD41F', '#BFD41F', '#F0D923', '#F8B520'
+        , '#F88C20', '#F84A20', '#F13B7F', '#FD808B',
+        '#F88C21'
+    ];
+    function changeColor() {
+        setisClicked(!isClicked)
 
     }
-
     return (
-        <div className="note">
-            <button onClick={changeCOlor}>//</button>
+        <>
+            <div className="note">
+                <button onClick={changeColor}>//</button>
+                {isClicked ?
+                    <div className="curr" >
+                        {mycolors.map((c, i) => {
+                            return <div className="divColors " className="colorDiv handPointer"
+                                // style={{ backgroundColor: c }}
+                                style={{ backgroundColor: c }}
+                            >
+                            </div>
+                        })}
+                    </div>
+                    : ""}
+            </div>
 
-        </div>
+        </>
     )
 
 
