7d6c3f6ecfb83cbfe3f558d254744e80a68f2e76
MiriErnfeld <miri@leader.codes>
2021-05-09 16:11:06 +0300

    --- Auto-Git Commit ---

--- src/components/myNote.js
+++ src/components/myNote.js
@@ -8,27 +8,15 @@ import $ from 'jquery'
 
 export default function Notes() {
 
-    useEffect(() => {
-        debugger
-        // setstyleNote({
-        //     right: randomBetween(0, window.innerWidth - 150) + 'px',
-        //     top: randomBetween(0, window.innerHeight - 150) + 'px',
-        //     transform: 'rotate( ' + randomBetween(-15, 15) + 'deg)'
-        // });
-        // console.log("dtyle---" + styleNote)
-        $('.note').draggable();
-    }, [])
-
-    const [isClicked, setisClicked] = useState(false)
-    const [indexNote, setindexNote] = useState("")
-    const [currentIndex, setcurrentIndex] = useState("")
-    const [btn, setbtn] = useState(false)
     let [arr, setarr] = useState([])
-    const [countNote, setcountNote] = useState(0)
-    const [styleNote, setstyleNote] = useState()
     const [arrnums, setarrnums] = useState([{}])
     const dispatch = useDispatch()
     const nums = [3, 7, 0, 9, 7, 4, 2, 14, 6, 23, 18, 29, 10, 2,]
+
+    useEffect(() => {
+        debugger
+        $('.note').draggable();
+    }, [])
 
     const randomBetween = (min = 1, max = 900) => {
         return (min + Math.ceil(Math.random() * max));
@@ -36,61 +24,51 @@ export default function Notes() {
 
     function insertNote(index) {
         debugger
-
-        let count = (countNote + 1)
-        setcountNote(countNote + 1)
-        setbtn(!btn)
-        console.log(countNote);
-        setarr([...arr, { text: "", color: "", count: countNote, flagColor: false }])
+        setarr([...arr, { text: "", color: "", flagColor: false }])
         setarrnums([...arrnums, { x: randomBetween(), y: randomBetween() }])
-        // dispatch(actions.addNote({}))
         debugger
-
     }
     const mycolors = [
         '#44D7B6', '#40D9ED', '#3598F4', '#8580FD', '#6236FC', '#B620E0', '#FD80E5', '#6DD41F', '#BFD41F', '#F0D923', '#F8B520'
         , '#F88C20', '#F84A20', '#F13B7F'
     ];
-    function changeColor(index, item) {
+    function openEditor(index, item) {
         debugger
-        // setarr([...arr, { text: "", color: "", count: countNote,flagColor:false }])
-        // arr[index].flagColor = true
-        let aa = [...arr]
-        aa[index].flagColor = !item.flagColor
-        // item.flagColor = !item.flagColor
-        setarr(aa)
-
-        console.log("arr[index].flagcoloer" + arr[index].flagColor);
-        console.log("isClicked" + isClicked);
-        // setcurrentIndex(arr[index].count)
-        // setindexNote(index)
-        // setisClicked(true)
+        let newArr = [...arr]
+        newArr[index].flagColor = !item.flagColor
+        setarr(newArr)
+    }
+    function changeColor(item, index) {
+        debugger;
+        $(`note ${index}`).css('backgroundColor', item)
     }
 
-    useEffect(() => { console.log('on use'); debugger; }, [arr])
-    function close(index) {
+    useEffect(() => {
+        console.log('on use'); debugger;
+        $('.note').draggable();
+    }, [arr])
+
+    function close(index, item) {
         debugger
+        console.log(arr);
         let list = [...arr]
-        var elm = list[index];
         list.splice(index, 1);
+        console.log(list);
         setarr([...list])
     }
     return (
         <>
             <button onClick={insertNote} >add note</button>
-            {/* {btn ?
-       <Notes></Notes> : " "} */}
             <div className="container">
                 <div className="">
                     {arr.map((item, index) =>
                         <>
-                            <div key={index} className="note note-container" style={{
+                            <div key={index} className=`note ${index}` style={{
                                 top: `${index * 30}px`,
                                 left: `${arrnums[index].x}px`
                             }}>
-
                                 <div className="header">
-                                    <BsPencil onClick={e => changeColor(index, item)} style={{
+                                    <BsPencil onClick={e => openEditor(index, item)} style={{
                                         marginLeft: "121px",
                                         marginTop: " 8px"
                                     }}></BsPencil>
@@ -98,33 +76,25 @@ export default function Notes() {
                                     <BsX style={{
                                         marginRight: "123px",
                                         marginTop: "-15px"
-                                    }} onClick={e => close(index)}></BsX>
+                                    }} onClick={e => close(index, item)}></BsX>
                                 </div>
                                 <div className="curr-container">
-
                                     {item.flagColor ?
                                         <div className="curr" >
-                                            {mycolors.map((c, i) => {
+                                            {mycolors.map((color, index) => {
                                                 return <div className="divColors " className="colorDiv handPointer"
-                                                    // style={{ backgroundColor: c }}
-                                                    style={{ backgroundColor: c }}
-                                                >
+                                                    style={{ backgroundColor: color }} onClick={e => changeColor(color, index)}>
                                                 </div>
                                             })}
                                         </div>
-                                        : "wwwww"}
+                                        : ""}
                                 </div>
                             </div>
-
                         </>)
                     }
-
-
                 </div>
             </div>
-
         </>
-
     )
 }
 
