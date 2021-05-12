26b1a44d58654af36cdf4f3339699aea366db3cd
MiriErnfeld <miri@leader.codes>
2021-05-12 10:01:50 +0300

    --- Auto-Git Commit ---

--- src/components/configurator.js
+++ src/components/configurator.js
@@ -1,12 +1,22 @@
 import React, { useState } from 'react'
 import './configurator.css'
+// import Container from 'react-bootstrap/Container'
+// import Row from 'react-bootstrap/Row'
+// import Col from 'react-bootstrap/Col'
 
 export default function Configurator() {
 
 
     return (
         <div >
-            <div className="headerConfig">ss</div>
+            {/* <Container>
+
+                <Row>
+                    <Col></Col>
+                    <Col></Col>
+                    <Col className="col3"></Col>
+                </Row>
+            </Container> */}
         </div>
     )
 
