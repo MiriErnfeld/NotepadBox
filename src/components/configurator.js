0492e06c12bf321f8520a40031871878437a38f8
MiriErnfeld <miri@leader.codes>
2021-05-20 13:40:36 +0300

    add responsive

--- src/components/configurator.js
+++ src/components/configurator.js
@@ -68,24 +68,24 @@ export default function Configurator() {
         <>
             <div className="container-notes">
                 <div className="configurator-line row justify-content-start d-flex ">
-                    <p className="my-notes">My Notes</p>
-                    <div class="col-2">
-                        {countCol > 0 ? <input type="text" className="inputTitle1" onChange={e => changeStyle(1)} /> : ""}
-                    </div>
-                    <div className="col-2">
-                        {countCol > 1 ? <input type="text" className="inputTitle2" onChange={e => changeStyle(2)} /> : ""}
-                    </div>
-                    <div className="col-2">
-                        {countCol > 2 ? <input type="text" className="inputTitle3" onChange={e => changeStyle(3)} /> : ""}
-                    </div>
-                    <div className="col-2">
-                        {countCol > 3 ? <input type="text" className="inputTitle4" onChange={e => changeStyle(4)} /> : " "}
-                    </div>
-                    <div className="col-s">
-                        {countCol > 4 ? <input type="text" className="inputTitle5" onChange={e => changeStyle(5)} /> : " "}
-                    </div> <p className="p-cloumn col-2" onClick={addCol}>
+                    <p className="my-notes col-2 ">My Notes</p>
+
+                    {countCol > 0 ? <div class="col-2" style={{ left: "-1%" }}> <input type="text" className="inputTitle1" onChange={e => changeStyle(1)} /> </div> : ""}
+
+                    {countCol > 1 ? <div className="col-2">
+                        <input type="text" className="inputTitle2" onChange={e => changeStyle(2)} />    </div> : ""}
+
+                    {countCol > 2 ? <div className="col-2">
+                        <input type="text" className="inputTitle3" onChange={e => changeStyle(3)} />        </div> : ""}
+
+                    {countCol > 3 ? <div className="col-2">
+                        <input type="text" className="inputTitle4" onChange={e => changeStyle(4)} />    </div> : " "}
+
+                    {countCol > 4 ? <div className="col-2">
+                        <input type="text" className="inputTitle5" onChange={e => changeStyle(5)} /> </div> : " "}
+                    <p className="p-cloumn col-2" onClick={addCol}>
                         new coloumn
-                    <BsFillPlusCircleFill className="plus-icon" ></BsFillPlusCircleFill>
+                    <BsFillPlusCircleFill className="col-1" className="plus-icon" ></BsFillPlusCircleFill>
                     </p>
                 </div>
                 <div class="container">
