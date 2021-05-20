06ea7705426b66dcc9c668fc54d3da856b8ad31c
MiriErnfeld <miri@leader.codes>
2021-05-20 16:14:01 +0300

    --- Auto-Git Commit ---

--- src/components/configurator.js
+++ src/components/configurator.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import $ from 'jquery'
 import './configurator.css'
+
 import { actions } from '../components/redux/actions/action';
 import folserPlus from '../images/folder-plus.png'
 import { useDispatch, useSelector } from 'react-redux'
@@ -8,8 +9,11 @@ import { FiFolderPlus, FiFolder, FiMoreVertical } from "react-icons/fi";
 import { FcPlus } from "react-icons/fc";
 import Dropdown from 'react-bootstrap/Dropdown'
 import { BsFillPlusCircleFill } from "react-icons/bs";
+
 // import 'bootstrap/dist/css/bootstrap.min.css';
+
 import MyNote from './myNote'
+var Color = require('color');
 
 export default function Configurator() {
     const [arr, setarr] = useState([])
@@ -25,9 +29,11 @@ export default function Configurator() {
     function addCol() {
         debugger
         if (countCol == 4) {
+
             $('.p-cloumn').css("display", "none")
         }
         let cnt = countCol + 1
+
         if (countCol < 5) {
             setCountCol(cnt)
         }
@@ -39,40 +45,51 @@ export default function Configurator() {
         $('.inputTitle' + index).css("font-weight", "bold");
         $('.inputTitle' + index).css("text-align", "center");
     }
+
     function insertNote() {
-        debugger
+
         let cnt = count + 1
         setCount(cnt)
+
         setarr([...arr, { text: "", check: false, flagColor: false, colors: "#FFEB3B", id: count, top: topNote, right: rightNote }])
         setarrnums([...arrnums, { x: randomBetween(), y: randomBetween() }])
+
         console.log(arr);
+
+
     }
+
     return (
         <>
             <div className="container-notes">
                 <div className="configurator-line row justify-content-start d-flex ">
-                    <p className="my-notes col-2 ">My Notes</p>
-                    {countCol > 0 ? <div class="col-2" style={{ left: "-1%" }}> <input type="text" className="inputTitle1" onChange={e => changeStyle(1)} /> </div> : ""}
+                    <p className="my-notes col-2">My Notes:</p>
+
+                    {countCol > 0 ? <div class="col-2"> <input type="text" className="inputTitle1" onChange={e => changeStyle(1)} /> </div> : ""}
+
                     {countCol > 1 ? <div className="col-2">
                         <input type="text" className="inputTitle2" onChange={e => changeStyle(2)} />    </div> : ""}
+
                     {countCol > 2 ? <div className="col-2">
-                        <input type="text" className="inputTitle3" onChange={e => changeStyle(3)} />        </div> : ""}
+                        <input type="text" className="inputTitle3" onChange={e => changeStyle(3)} />   </div> : ""}
+
                     {countCol > 3 ? <div className="col-2">
                         <input type="text" className="inputTitle4" onChange={e => changeStyle(4)} />    </div> : " "}
-                    {countCol > 4 ? <div className="col-2">
+
+                    {countCol > 4 ? <div className="col-s">
                         <input type="text" className="inputTitle5" onChange={e => changeStyle(5)} /> </div> : " "}
                     <p className="p-cloumn col-2" onClick={addCol}>
                         new coloumn
-                    <BsFillPlusCircleFill className="col-1" className="plus-icon" ></BsFillPlusCircleFill>
+                    <BsFillPlusCircleFill className="plus-icon" ></BsFillPlusCircleFill>
                     </p>
                 </div>
                 <div class="container">
                     <div class="row">
                         <div class="col-sm-2" style={{ borderRight: "4px solid #dee2e6", height: "550px" }}>
                         </div>
-                        {countCol > 0 ? <div class="col-sm-2" style={{ borderRight: "4px solid #dee2e6", height: "550px" }}>
+                        {countCol > 0 ? <div class="col-sm-2   " style={{ borderRight: "4px solid #dee2e6", height: "550px" }}>
                         </div> : " "}
-                        {countCol > 1 ? <div class="col-sm-2" style={{ borderRight: "4px solid #dee2e6", height: "550px" }}>
+                        {countCol > 1 ? <div class="col-sm-2 " style={{ borderRight: "4px solid #dee2e6", height: "550px" }}>
                         </div> : ""}
                         {countCol > 2 ? <div class="col-sm-2 " style={{ borderRight: "4px solid #dee2e6", height: "550px" }}>
                         </div> : ""}
@@ -93,6 +110,7 @@ export default function Configurator() {
                     folder name */}
                     </div>
                 </div>
+
             </div >
             <MyNote
                 arr={arr}
@@ -106,7 +124,7 @@ export default function Configurator() {
                 rightNote={rightNote}
                 setRightNote={setRightNote}
             ></MyNote>
-
         </>
     )
+
 }
--- src/components/myNote.js
+++ src/components/myNote.js
@@ -25,7 +25,9 @@ export default function Notes(props) {
     const dispatch = useDispatch()
     const nums = [300, 7, 0, 9, 7, 4, 2, 14, 6, 23, 18, 29, 10, 2,]
 
-
+    useEffect(() => {
+        console.log(arr);
+    }, [arr])
     const mycolors = [
         '#F84A20', '#F13B7F', '#F88C20', '#FD808B', '#F8DB3D', '#B620E0',
         '#BFD41F', '#8580FD', '#6DD41F', '#7bdcb5', '#44D7B6'
@@ -92,68 +94,68 @@ export default function Notes(props) {
     return (
         <>
             {/* <div className="container"> */}
-                <div className="all-notes">
-                    {arr.map((item, index) =>
-                        <>
-                            <div key={index} className={`note ${item.id}`}
-                                style={{
-                                    top: `${index * 30 + 50}px`,
-                                    right: `${props.arrnums[index].x + 300}px`
-                                }}>
-                                {setTopNote(`${index * 30 + 50}px`)}
-                                {setRightNote(`${props.arrnums[index].x + 300}px`)}
-                                <div className={`header ${item.id}`}
-                                    style={{ backgroundColor: LightenDarkenColor(item.colors, -45) }}>
-                                    <BsPencil
-                                        // onMouseEnter={e => closeEditor(item)}
-                                        onClick={() => openCloseEditor(item)}
-                                        style={{
-                                            color: "#0A102E",
-                                            marginLeft: "121px",
-                                            marginTop: " 8px",
-                                            paddingBottom: "3px",
-                                            cursor: "auto",
-                                        }}></BsPencil>
-                                    <FaGripHorizontal style={{ marginTop: "-16px", fontWeight: "none", color: "#0A102E" }}></FaGripHorizontal>
-                                    <BsX style={{
+            <div className="all-notes">
+                {arr.map((item, index) =>
+                    <>
+                        <div key={index} className={`note ${item.id}`}
+                            style={{
+                                top: `${index * 3}px`,
+                                left: `${index + 6}px`
+                            }}>
+                            {setTopNote(`${index * 30 + 50}px`)}
+                            {setRightNote(`${props.arrnums[index].x + 300}px`)}
+                            <div className={`header ${item.id}`}
+                                style={{ backgroundColor: LightenDarkenColor(item.colors, -45) }}>
+                                <BsPencil
+                                    // onMouseEnter={e => closeEditor(item)}
+                                    onClick={() => openCloseEditor(item)}
+                                    style={{
                                         color: "#0A102E",
-                                        hoverBackground: "black",
-                                        marginRight: "123px",
+                                        marginLeft: "121px",
+                                        marginTop: " 8px",
                                         paddingBottom: "3px",
                                         cursor: "auto",
-                                        marginTop: "-15px"
-                                    }} onClick={() => removeItem(item)}></BsX>
-                                    <textarea
-                                        className={`textarea ${item.id}`}
-                                        id="areaText"
-                                        type="string"
-                                        onBlur={e => saveText(item, e.target.value)}
-                                    ></textarea>
-                                </div>
-                                <div className="curr-container">
-                                    {item.flagColor ?
-                                        <div className="curr" >
-                                            {mycolors.map((c, i) => {
-                                                return <div key={i} className="divColors " className="colorDiv handPointer"
-                                                    style={{ backgroundColor: c }} onClick={e => changeColor(c, item, i)}
-                                                > {check == i && currentItem == item ?
-                                                    <BsCheck
-                                                        style={{
-                                                            fontSize: "13px",
-                                                            marginTop: " 2px",
-                                                            color: "white",
-                                                            fontWeight: "bold"
-                                                        }}></BsCheck> : " "}
-                                                </div>
-
-                                            })}
-                                        </div>
-                                        : ""}
-                                </div>
+                                    }}></BsPencil>
+                                <FaGripHorizontal style={{ marginTop: "-16px", fontWeight: "none", color: "#0A102E" }}></FaGripHorizontal>
+                                <BsX style={{
+                                    color: "#0A102E",
+                                    hoverBackground: "black",
+                                    marginRight: "123px",
+                                    paddingBottom: "3px",
+                                    cursor: "auto",
+                                    marginTop: "-15px"
+                                }} onClick={() => removeItem(item)}></BsX>
+                                <textarea
+                                    className={`textarea ${item.id}`}
+                                    id="areaText"
+                                    type="string"
+                                    onBlur={e => saveText(item, e.target.value)}
+                                ></textarea>
                             </div>
-                        </>)
-                    }
-                </div>
+                            <div className="curr-container">
+                                {item.flagColor ?
+                                    <div className="curr" >
+                                        {mycolors.map((c, i) => {
+                                            return <div key={i} className="divColors " className="colorDiv handPointer"
+                                                style={{ backgroundColor: c }} onClick={e => changeColor(c, item, i)}
+                                            > {check == i && currentItem == item ?
+                                                <BsCheck
+                                                    style={{
+                                                        fontSize: "13px",
+                                                        marginTop: " 2px",
+                                                        color: "white",
+                                                        fontWeight: "bold"
+                                                    }}></BsCheck> : " "}
+                                            </div>
+
+                                        })}
+                                    </div>
+                                    : ""}
+                            </div>
+                        </div>
+                    </>)
+                }
+            </div>
             {/* </div> */}
         </>
     )
